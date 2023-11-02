24f895cb6c2f2c5be61fa5caa01bbd834a15b550
Miriam Wisky <miriamwisky11@gmail.com>
2023-11-02 13:18:26 +0200

    version 2

--- semantic_func.py
+++ semantic_func.py
@@ -58,6 +58,7 @@ def calculate_semantic_similarity():
     word1 = word1.lower()
     word2 = word2.lower()
     if word1 == word2:
+        print("same")
         return jsonify({'similarity': 1})
 
     # Get the word vectors
